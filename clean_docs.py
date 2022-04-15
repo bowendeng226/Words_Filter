58f0872be68d52133fec57b2edf7ac3938edf8ba
Deng <deng6198@qq.com>
2022-04-15 19:20:28 +0800

    Add test to function

--- clean_docs.py
+++ clean_docs.py
@@ -1,24 +1,31 @@
-'''
-去除txt文档里的多余空格、换行符、制表符等；
-将文档里的单词全部小写
-去除文档里的单字母
-去除数字 maybe...
-'''
 
 
-half_punctuations = "`~!@#$%^&*()-_=+[{]}\|;:'" + '",<.>/?'
-
-full_punctuations = "·~！@#￥%……&*（）-——=+【{】}、|；：‘’“”，《。》/？"
-
-punctuations = half_punctuations + full_punctuations
 
 # file_name = "C:\\Users\\bowen\\desktop\\L3_unformated.txt"
 
 
 def clean_doc(file_name):
+    """
+    去除txt文档里的去除txt文档里的多余空格、换行符、制表符等；
+    将文档里的单词全部小写
+    去除文档里的单字母
+    去除数字 （未开发...）
+    返回一个列表
 
-    '''描述见文件顶部'''
+    Args:
+        file_name (_txt_): _接收一个txt文档_
 
+    Returns:
+        _list_: _返回包含该txt文档所有符合条件单词的列表_
+    """
+
+    # 全角和半角标点符号
+
+    half_punctuations = "`~!@#$%^&*()-_=+[{]}\|;:'" + '",<.>/?'
+    full_punctuations = "·~！@#￥%……&*（）-——=+【{】}、|；：‘’“”，《。》/？"
+    punctuations = half_punctuations + full_punctuations
+
+    # 去除文档里的标点符号
 
     with open(file_name) as f_obj:
 
@@ -28,7 +35,7 @@ def clean_doc(file_name):
             if p in contents:
                 contents = contents.replace(p, ' ')
 
-    # 将contents转为列表
+    # 将单词转为列表
 
     content_list = contents.split()
 
@@ -44,7 +51,6 @@ def clean_doc(file_name):
     for i in list_clean[:]:
         if i == ' ' or len(i) < 4:
             list_clean.remove(i)
-
 
     return list_clean
 
